47f77c2d25d1feda1da0abef6df17464b51b4939
Mert-Ozbolat <mertozbolat008@gmail.com>
2025-01-25 22:38:01 +0300

    navigation added

--- src/router/rootNavigator.jsx
+++ src/router/rootNavigator.jsx
@@ -1,11 +1,17 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { TABNAVIGATOR } from '../utils/routes';
+import Tabnavigator from './tabNavigator';
 
 const Stack = createNativeStackNavigator();
 
 function RootNavigator() {
     return (
-        <Stack.Navigator>
-            <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Navigator
+            screenOptions={{
+                headerShown: false
+            }}
+        >
+            <Stack.Screen name={TABNAVIGATOR} component={Tabnavigator} />
         </Stack.Navigator>
     );
 }
--- src/router/tabNavigator.jsx
+++ src/router/tabNavigator.jsx
@@ -1,12 +1,17 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { CONTACTS, FAVORITES, RESENTS } from '../utils/routes';
+import Resents from '../screens/resents';
+import Contacts from '../screens/contacts';
+import Favorites from '../screens/favorites';
 
 const Tab = createBottomTabNavigator();
 
 function Tabnavigator() {
     return (
         <Tab.Navigator>
-            <Tab.Screen name="Home" component={HomeScreen} />
-            <Tab.Screen name="Profile" component={ProfileScreen} />
+            <Tab.Screen name={RESENTS} component={Resents} />
+            <Tab.Screen name={CONTACTS} component={Contacts} />
+            <Tab.Screen name={FAVORITES} component={Favorites} />
         </Tab.Navigator>
     );
 }
